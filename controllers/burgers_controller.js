91dd67ec58af3e7af3c2ee5b09fc904fa38272fa
Mubeena Ahmed <beena4jaz@gmail.com>
2018-02-21 13:28:27 -0600

    working files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,30 +16,24 @@ router.get("/", function(req,res){
 	});
 });
 
-router.post("/api/burgers", function(req,res){
+router.post("/burgers", function(req,res){
 	burger.insertOne([
-		"burger_name", "devoured"
+		"burger_name"
 	], [
-	 	req.body.burger_name, req.body.devoured
-	], function(result){
+	 	req.body.burger_name
+	], function(data){
 		//Send back the ID of the new burger
-		res.json({ id: result.insertId });
+		res.redirect("/");
 	});
 });
 
-router.put("/api/burgers/:id", function(req,res){
+router.put("/burgers/:id", function(req,res){
 	var condition = "id = " + req.params.id;
 
-	console.log("condition", condition);
-
 	burger.updateOne({
-		devoured: req.body.devoured
-	}, condition, function(result) {
-		if (result.changedRows == 0) {
-			return res.status(404).end();
-		} else{
-			res.status(200).end();
-		}
+		devoured: true
+	}, condition, function(data) {
+		res.redirect("/");
 	});
 });
 
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(methodOverride('_method'));
+
 //Set Handlebars
 var exphbs = require("express-handlebars");
 
@@ -20,7 +22,7 @@ app.set("view engine", "handlebars");
 //Import routes and give the server access to them
 var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+app.use("/", routes);
 
 app.listen(PORT, function(){
 	console.log("App now listening at localhost:" + PORT);
